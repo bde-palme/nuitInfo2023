a11d64355631c1a91b6484398e3021dc260ffa0f
Natounet <naytan@protonmail.com>
2023-12-11 10:27:14 +0100

    formatting

--- api/api.ts
+++ api/api.ts
@@ -23,260 +23,277 @@ app.use((req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-  });
+});
 
 app.get('/', (req, res) => {
-    res.send('API - NDLI 2023 - La PALME');
+  res.send('API - NDLI 2023 - La PALME');
 });
 
 app.get('/add_team.html', (req, res) => {
-    res.sendFile(__dirname + '/static/add_team.html');
+  res.sendFile(__dirname + '/static/add_team.html');
 });
 
 app.get('/add_user.html', (req, res) => {
-    res.sendFile(__dirname + '/static/add_user.html');
+  res.sendFile(__dirname + '/static/add_user.html');
 });
 
 
 
 app.get('/api/users/', async (req, res) => {
-    const token = req.query.token;
+  const token = req.query.token;
 
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
+
+  const users = await lib.get_all_users();
+  res.send(users);
+});
+
+app.get('/api/teams/', async (req, res) => {
+  const token = req.query.token;
+
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
+
+  const teams = await lib.get_all_teams();
+  res.send(teams);
+});
+
+app.post('/api/teams/create', async (req, res) => {
+  const teamName = req.body.teamName;
+  const teamHash = req.body.teamHash;
+
+  if (!teamName || !teamHash) {
+    res.status(400).send('Team name and hash are required');
+    return;
+  }
+
+  const teamExists = await lib.team_exist(teamName);
+  if (teamExists) {
+    res.status(400).send('Team already exists');
+    return;
+  }
+
+  const team = {
+    name: teamName,
+    hash: teamHash,
+    members: [] // Fix: Corrected property name from 'membres' to 'members'
+  };
+
+  await lib.add_team(team);
+  res.status(201).send('Team created');
+});
+
+app.post('/api/users/create', async (req, res) => {
+
+  if (req.body.course == "true") {
+    req.body.course = true;
+  }
+  else {
+    req.body.course = false;
+  }
+
+  if (req.body.pmr == "true") {
+    req.body.pmr = true;
+  }
+  else {
+    req.body.pmr = false;
+  }
+
+  const user: lib.User = {
+    name: req.body.name,
+    first_name: req.body.first_name,
+    pmr: req.body.pmr,
+    course: req.body.course,
+    teacher: req.body.teacher,
+    timestamp: new Date().toISOString(), // Fix: Convert the timestamp to a string using toISOString()
+    email: req.body.email,
+    nickname: req.body.nickname,
+    phone: req.body.phone,
+    study: req.body.study,
+    comment: req.body.comment
+  };
+
+  const teamName = req.body.teamName;
+  const teamPass = req.body.teamPassword;
+  console.log(req.body)
+
+  // Check if all parameters are provided and are of the correct type
+  if (typeof user.name !== 'string' || typeof user.first_name !== 'string' ||
+    typeof user.pmr !== 'boolean' || typeof user.course !== 'boolean' ||
+    typeof user.teacher !== 'string' ||
+    typeof user.email !== 'string' || typeof user.nickname !== 'string' ||
+    typeof user.phone !== 'string' || typeof user.study !== 'string' ||
+    typeof user.comment !== 'string' || typeof teamName !== 'string' ||
+    typeof teamPass !== 'string') {
+
+    res.status(400).send('Invalid parameters');
+    return;
+  }
+
+  const userExists = await lib.user_exist(user.email, user.phone, user.nickname);
+  if (userExists) {
+    res.status(400).send('User already exists');
+    return;
+  }
+
+  const teamExists = await lib.team_exist(teamName);
+  if (teamExists) {
+    const isValidHash = await lib.compare_team_password(teamPass, teamName);
+    if (isValidHash) {
+      await lib.add_user(user);
+      await lib.add_user_to_team(teamName, user);
+    } else {
+      res.status(400).send('Invalid team hash or team name');
       return;
     }
-  
-    const users = await lib.get_all_users();
-    res.send(users);
-  });
+  }
 
-  app.get('/api/teams/', async (req, res) => {
-    const token = req.query.token;
-  
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
-  
-    const teams = await lib.get_all_teams();
-    res.send(teams);
-  });
 
-  app.post('/api/teams/create', async (req, res) => {
-    const teamName = req.body.teamName;
-    const teamHash = req.body.teamHash;
-  
-    if (!teamName || !teamHash) {
-      res.status(400).send('Team name and hash are required');
-      return;
-    }
-  
-    const teamExists = await lib.team_exist(teamName);
-    if (teamExists) {
-        res.status(400).send('Team already exists');
-        return;
-    }
+  res.status(201).send('User created and added to team');
+});
 
-    const team = {
-        name: teamName,
-        hash: teamHash,
-        members: [] // Fix: Corrected property name from 'membres' to 'members'
-    };
+app.get('/api/users/count', async (req, res) => {
+  const token = req.query.token;
 
-    await lib.add_team(team);
-    res.status(201).send('Team created');
-  });
- 
-  app.post('/api/users/create', async (req, res) => {
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
 
-    if(req.body.course == "true"){
-      req.body.course = true;
-    }
-    else{
-      req.body.course = false;
-    }
+  const count = await lib.get_number_of_users();
+  res.send({ count });
+});
 
-    if(req.body.pmr == "true"){
-      req.body.pmr = true;
-    }
-    else{
-      req.body.pmr = false;
-    }
+app.get('/api/teams/count', async (req, res) => {
+  const token = req.query.token;
 
-    const user: lib.User = {
-      name: req.body.name,
-      first_name: req.body.first_name,
-      pmr: req.body.pmr,
-      course: req.body.course,
-      teacher: req.body.teacher,
-      timestamp: new Date().toISOString(), // Fix: Convert the timestamp to a string using toISOString()
-      email: req.body.email,
-      nickname: req.body.nickname,
-      phone: req.body.phone,
-      study: req.body.study,
-      comment: req.body.comment
-    };
-  
-    const teamName = req.body.teamName;
-    const teamPass = req.body.teamPassword;
-    console.log(req.body)
-  
-    // Check if all parameters are provided and are of the correct type
-    if (typeof user.name !== 'string' || typeof user.first_name !== 'string' || 
-        typeof user.pmr !== 'boolean' || typeof user.course !== 'boolean' || 
-        typeof user.teacher !== 'string' ||  
-        typeof user.email !== 'string' || typeof user.nickname !== 'string' || 
-        typeof user.phone !== 'string' || typeof user.study !== 'string' || 
-        typeof user.comment !== 'string' || typeof teamName !== 'string' || 
-        typeof teamPass !== 'string') {
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
 
-      res.status(400).send('Invalid parameters');
-      return;
-    }
-  
-    const userExists = await lib.user_exist(user.email, user.phone, user.nickname);
-    if (userExists) {
-      res.status(400).send('User already exists');
-      return;
-    }
-  
-    const teamExists = await lib.team_exist(teamName);
-    if (teamExists) {
-      const isValidHash = await lib.compare_team_password(teamPass,teamName);
-      if (isValidHash) {
-        await lib.add_user(user);
-        await lib.add_user_to_team(teamName, user);
-      } else {
-        res.status(400).send('Invalid team hash or team name');
-        return;
-      }
-    }
+  const count = await lib.get_number_of_teams();
+  res.send({ count });
+});
 
-  
-    res.status(201).send('User created and added to team');
-  });
+app.get('/api/teams/:teamName/count', async (req, res) => {
+  const token = req.query.token;
 
-  app.get('/api/users/count', async (req, res) => {
-    const token = req.query.token;
-  
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
-  
-    const count = await lib.get_number_of_users();
-    res.send({ count });
-  });
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
 
-  app.get('/api/teams/count', async (req, res) => {
-    const token = req.query.token;
-  
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
-  
-    const count = await lib.get_number_of_teams();
-    res.send({ count });
-  });
+  const teamName = req.params.teamName;
+  // Check if team exists
+  const teamExists = await lib.team_exist(teamName);
+  if (!teamExists) {
+    res.status(404).send('Team not found');
+    return;
+  }
 
-  app.get('/api/teams/:teamName/count', async (req, res) => {
-    const token = req.query.token;
-  
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
-  
-    const teamName = req.params.teamName;
-    // Check if team exists
-    const teamExists = await lib.team_exist(teamName);
-    if (!teamExists) {
-      res.status(404).send('Team not found');
-      return;
-    }
+  const count = await lib.get_number_of_members(teamName);
+  res.send({ count });
+});
 
-    const count = await lib.get_number_of_members(teamName);
-    res.send({ count });
-  });
+app.get('/api/users/:username', async (req, res) => {
+  const token = req.query.token;
 
-  app.get('/api/users/:username', async (req, res) => {
-    const token = req.query.token;
-  
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
 
-  
-    const username = req.params.username;
-    const user = await lib.get_user(username);
-  
-    if (user === null) {
-      res.status(404).send('User not found');
-      return;
-    }
-  
-    res.send(user);
-  });
 
-  app.get('/api/teams/:teamName', async (req, res) => {
-    const token = req.query.token;
-  
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
-  
-    const teamName = req.params.teamName;
-    const team = await lib.get_team(teamName);
-  
-    if (team === null) {
-      res.status(404).send('Team not found');
-      return;
-    }
-  
-    res.send(team);
-  });
+  const username = req.params.username;
+  const user = await lib.get_user(username);
 
-  app.get('/api/users/:username/remove', async (req, res) => {
-    const token = req.query.token;
+  if (user === null) {
+    res.status(404).send('User not found');
+    return;
+  }
 
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
+  res.send(user);
+});
 
-    const username = req.params.username;
-    const user = await lib.get_user(username);
+app.get('/api/teams/:teamName', async (req, res) => {
+  const token = req.query.token;
 
-    if (user === null) {
-      res.status(404).send('User not found');
-      return;
-    }
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
 
-    await lib.delete_user(username);
-    res.send('User removed');
-  });
+  const teamName = req.params.teamName;
+  const team = await lib.get_team(teamName);
 
-  app.get('/api/teams/:teamName/remove', async (req, res) => {
-    const token = req.query.token;
+  if (team === null) {
+    res.status(404).send('Team not found');
+    return;
+  }
 
-    if (token !== api_token) {
-      res.status(403).send('Invalid token');
-      return;
-    }
+  res.send(team);
+});
 
-    const teamName = req.params.teamName;
-    const team = await lib.get_team(teamName);
+app.get('/api/users/:username/remove', async (req, res) => {
+  const token = req.query.token;
 
-    if (team === null) {
-      res.status(404).send('Team not found');
-      return;
-    }
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
 
-    await lib.delete_team(teamName);
-    res.send('Team removed');
-  });
+  const username = req.params.username;
+  const user = await lib.get_user(username);
+
+  if (user === null) {
+    res.status(404).send('User not found');
+    return;
+  }
+
+  await lib.delete_user(username);
+  res.send('User removed');
+});
+
+app.get('/api/teams/:teamName/remove', async (req, res) => {
+  const token = req.query.token;
+
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
+
+  const teamName = req.params.teamName;
+  const team = await lib.get_team(teamName);
+
+  if (team === null) {
+    res.status(404).send('Team not found');
+    return;
+  }
+
+  await lib.delete_team(teamName);
+  res.send('Team removed');
+});
+
+app.get('/api/emailAlreadyUsed/:email', async (req, res) => {
+  const email = req.params.email;
+  const isUsed = await lib.isEmailUsed(email);
+  res.send(isUsed);
+});
+app.get('/api/submissionInformation/', async (req, res) => {
+  const token = req.query.token;
+
+  if (token !== api_token) {
+    res.status(403).send('Invalid token');
+    return;
+  }
+
+  const submissionInfo = await lib.get_submission_info();
+  res.send(submissionInfo);
+});
 
